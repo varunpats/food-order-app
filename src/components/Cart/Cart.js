ded5506c6b1e4f7691c40be008a4bf2e82ebefde
varunpats <varunpatil2502@gmail.com>
2023-05-11 20:13:41 +0530

    Made cart functional

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import classes from './Cart.module.css';
+import Modal from '../UI/Modal';
 
 const Cart = props => {
     const cartItems = (<ul className={classes['cart-items']}>
@@ -6,17 +7,19 @@ const Cart = props => {
             <li>{item.name}</li>
         ))}
     </ul>);
-    <div>
+    return (
+    <Modal onClose={props.onShowCart}>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
             <span>35.82</span>
         </div>
-        <div>
-            <button className={classes['button--alt']}>Close</button>
+        <div className={classes.actions}>
+            <button className={classes['button--alt']} onClick={props.onShowCart}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
-    </div>
+    </Modal>
+    )
 }
 
 export default Cart;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
     return <React.Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
         <div className={classes["main-image"]}>
             <img src={mealImage} alt="A table full of delicious food!" />
