280b89493e8210885fee2d324adb2743b061a1e1
varunpats <varunpatil2502@gmail.com>
2023-05-17 22:42:02 +0530

    Adding redux

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,33 @@
+import { useReducer, useState } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if(action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+    return defaultCartState
+}
+
 const CartProvider = props => {
-    const addItemToCartHandler = item => { }
+    const [cartState, dispathCartAction] = useReducer(cartReducer, defaultCartState);
+    const addItemToCartHandler = item => {
+        dispathCartAction({ type: 'ADD', item: item });
+    }
     const removeItemFromCartHandler = id => { }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     }
